d4d99d7e7dc16bf53a72296646ed849df93267c7
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-05-08 01:06:23 -0300

    Inserindo comentarios aos posts

--- src/main/java/com/mateuspessoa/springmongo/config/Instantiation.java
+++ src/main/java/com/mateuspessoa/springmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.mateuspessoa.springmongo.domain.Post;
 import com.mateuspessoa.springmongo.domain.User;
 import com.mateuspessoa.springmongo.dto.AuthorDTO;
+import com.mateuspessoa.springmongo.dto.CommentDTO;
 import com.mateuspessoa.springmongo.repository.PostRepository;
 import com.mateuspessoa.springmongo.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("07/05/2022"), "Partiu Viagem", "Vou viajar para João Pessoa!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("08/05/2022"), "Boa Tarde", "Um ótimo dia a todos!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa Viagem, cara!", sdf.parse("08/05/2022"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite o passeio!", sdf.parse("08/05/2022"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um bom dia!", sdf.parse("08/05/2022"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
