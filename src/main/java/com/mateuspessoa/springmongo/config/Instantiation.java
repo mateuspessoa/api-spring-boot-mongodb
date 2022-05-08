be6aa5127975c2f8483441c2c27d0f0e8f5b2469
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-05-07 22:06:22 -0300

    Referenciando os posts dos usuários

--- src/main/java/com/mateuspessoa/springmongo/config/Instantiation.java
+++ src/main/java/com/mateuspessoa/springmongo/config/Instantiation.java
@@ -42,6 +42,9 @@ public class Instantiation implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("08/05/2022"), "Boa Tarde", "Um ótimo dia a todos!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 
 		
 	}
